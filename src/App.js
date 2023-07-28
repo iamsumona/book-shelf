a51e8f9b2f599319a1589dc49de4ffb51fb5de5f
iamsumona <sumona7sumu@gmail.com>
2023-07-28 19:24:28 +0600

    setup project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/routes'
+
+function App() {
+
+return <div className='max-w-screen-xl mx-auto'><RouterProvider router={router} /></div>
+
+
+
+  
+}
+
+export default App
